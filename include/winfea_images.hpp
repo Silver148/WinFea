0325f4037689ffdd3c6d2061b1de0d3c15858bf7
Silver148 <juanjoseyaguaro65@gmail.com>
2026-08-31 21:11:18 -0400

    refactor: remove trailing underscores from WinFeaImage member variables

--- include/winfea_images.hpp
+++ include/winfea_images.hpp
@@ -4,32 +4,32 @@
 
 class WinFeaImage : public WinFea::RenderableObject {
 private:
-    SDL_Texture* texture_ = nullptr;
-    SDL_FRect rect_ = {0.0f, 0.0f, 0.0f, 0.0f};
+    SDL_Texture* texture = nullptr;
+    SDL_FRect rect = {0.0f, 0.0f, 0.0f, 0.0f};
 
 public:
     ~WinFeaImage() override {
-        if(texture_){
-            SDL_DestroyTexture(texture_);
-            texture_ = nullptr;
+        if(texture){
+            SDL_DestroyTexture(texture);
+            texture = nullptr;
         }
     }
 
     void Render(render r) const override {
-        if(texture_){
-            SDL_RenderTexture(r, texture_, nullptr, &rect_);
+        if(texture){
+            SDL_RenderTexture(r, texture, nullptr, &rect);
         }
     }
 
     SDL_Texture* ShowImage(render r, std::string path, float x, float y);
     SDL_Texture* RedimensionTexture(float w, float h);
-    SDL_Texture* GetTexture() const { return texture_; }
+    SDL_Texture* GetTexture() const { return texture; }
 
     void SetRect(float x, float y, float w, float h) {
-        rect_.x = x;
-        rect_.y = y;
-        rect_.w = w;
-        rect_.h = h;
+        rect.x = x;
+        rect.y = y;
+        rect.w = w;
+        rect.h = h;
     }
 };
 
--- src/winfea_images.cpp
+++ src/winfea_images.cpp
@@ -1,15 +1,15 @@
 #include "winfea_images.hpp"
 
 SDL_Texture* WinFeaImage::ShowImage(render r, std::string path, float x, float y){
-    if(texture_){
-        SDL_DestroyTexture(texture_);
-        texture_ = nullptr;
+    if(texture){
+        SDL_DestroyTexture(texture);
+        texture = nullptr;
     }
 
-    texture_ = IMG_LoadTexture(r, path.c_str());
-    rect_ = {0.0f, 0.0f, 0.0f, 0.0f};
+    texture = IMG_LoadTexture(r, path.c_str());
+    rect = {0.0f, 0.0f, 0.0f, 0.0f};
 
-    if(!texture_){
+    if(!texture){
         std::cout << "Failed to load image '" << path << "': " << SDL_GetError() << std::endl;
         return nullptr;
     }
@@ -17,15 +17,15 @@ SDL_Texture* WinFeaImage::ShowImage(render r, std::string path, float x, float y
     float tw = 0.0f;
     float th = 0.0f;
 
-    if(SDL_GetTextureSize(texture_, &tw, &th)){
+    if(SDL_GetTextureSize(texture, &tw, &th)){
         SetRect(x, y, tw, th);
     }
 
-    return texture_;
+    return texture;
 }
 
 SDL_Texture* WinFeaImage::RedimensionTexture(float w, float h){
-    rect_.w = w;
-    rect_.h = h;
-    return texture_;
+    rect.w = w;
+    rect.h = h;
+    return texture;
 }
